ce5a996a4fea4d1e30d3d64437bdaeee35c398d1
Mohammad-Gharbi <91133928+Mohammad-Gharbi@users.noreply.github.com>
2023-08-25 00:17:53 +0100

    testing

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -16,7 +16,7 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger data-testid="trigger" asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -24,13 +24,16 @@ export function ModeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem data-testid="light" onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem className="dark" onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem
+          data-testid="system"
+          onClick={() => setTheme("system")}
+        >
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
---
+++ tests/mode-toggle.test.js
@@ -0,0 +1,27 @@
+import {
+  fireEvent,
+  getByText,
+  getByTitle,
+  render,
+  screen,
+} from "@testing-library/react"
+import { ModeToggle } from "@/components/mode-toggle"
+import { ThemeProvider } from "next-themes"
+
+it("Switches The Theme", async () => {
+  const { getByTestId, getAllByTestId } = render(<ModeToggle />)
+  const toggle = getByTestId("trigger")
+
+  // fireEvent.click(toggle)
+
+  // const darkToggle = getByTestId("dark").fireEvent.click(darkToggle)
+  expect(localStorage.getItem("theme")).toEqual("dark")
+
+  // const lightToggle = screen.getByTestId("light")
+  // fireEvent.click(lightToggle)
+  // expect(localStorage.getItem("theme")).toBe("light")
+
+  // const systemToggle = screen.getByTestId("system")
+  // fireEvent.click(systemToggle)
+  // expect(localStorage.getItem("theme")).toBe("system")
+})
